66e81c3d65c81cd766e4ce933328ea15a6685c4f
varsha_the_queen-Git <chebroluvarsha@gmail.com>
2022-12-29 09:46:25 +0530

    Setup

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
+const port = process.env.PORT || 3007
 
 //Define path for Express config
 const publicDirectory = path.join(__dirname,'../public')
@@ -104,7 +105,7 @@ app.get('*',(req,res)=>{
    })
 })
 
-app.listen(3007,()=>{
-    console.log('Server is up on port 3007.')
+app.listen(port,()=>{
+    console.log('Server is up on port ' + port)
 })
 
